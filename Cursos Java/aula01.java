8127be54ac17af693f1ccd7d88ff09e44e3cac5f
KleytonDaSilva <32545345+KleytonDaSilva@users.noreply.github.com>
2022-04-25 15:38:47 -0300

    Update aula01.java

--- Cursos Java/aula01.java
+++ Cursos Java/aula01.java
@@ -2,5 +2,6 @@ public class aula01 {
     public static void main(String[] args) {
 
         System.out.println("hello WORD!!");
+        System.out.println("mudificação dentro do github");
     }
 }
