cef2dd41c313b08d361b89a9cfae5fd80acd0d98
Kleyton <nksaddam@gmail.com>
2022-04-25 15:51:50 -0300

    nova_commit

--- Cursos Java/aula01.java
+++ Cursos Java/aula01.java
@@ -2,6 +2,6 @@ public class aula01 {
     public static void main(String[] args) {
 
         System.out.println("hello WORD!!");
-        System.out.println("mudificação dentro do github");
+        System.out.println("mudar dentro do github");
     }
 }
